8c58f9f8608cc6be04f22ffc117ffadf22709e5c
waqas-codes <muhammadwaqas96845@gmail.com>
2026-09-24 03:01:56 +0500

    video and user model added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -75,9 +75,9 @@ userSchema.methods.generateRefreshToken = function () {
         {
             _id: this._id
         },
-        process.env.REFRESH_TOKEN_SECRET,
+        // process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_SECRET
         }
     )
 }
